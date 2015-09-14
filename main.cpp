ee22ea13aa3a0250022550bff98f93cc9745c585
petterdj <petterdjupfeldt@gmail.com>
2015-09-14 22:12:21 +0200

    Snapshot. Switching to using character objects.

--- main.cpp
+++ main.cpp
@@ -11,19 +11,13 @@
 #include "utils/constants.h"
 #include "utils/physics.h"
 #include "block.hpp"
+#include "character.hpp"
 
 using namespace terminalGame;
 
 float _terminalVelocity = 0;
 float _timestep = 0.01;
 
-float _yPosition = 10;
-float _xPosition = 10;
-float _yVelocity = 0;
-float _xVelocity = 0;
-float _yAcceleration = 0;
-float _xAcceleration = 0;
-int _facing = RIGHT;
 int _directionKeyTimer = 0;
 
 int _heavyAttackTimer = 0;
@@ -32,90 +26,92 @@ int _lightAttackTimer = 0;
 std::vector<Block*> _levelVector;
 Block* _level[HEIGHT][WIDTH];
 
-bool atRest() {
-  int y = int(_yPosition);
-  int x = int(_xPosition);
+Character* _player;
+
+bool atRest(Character* character) {
+  int y = int(character->getYPosition());
+  int x = int(character->getXPosition());
   Block* block = _level[y+1][x];
   if (block == 0 || (block != 0 && block->isPassableFromAbove())) return false;
   return true;
 }
 
-void upkey() {
-  if (atRest()) {
-    _yVelocity = -10;
+void upkey(Character* character) {
+  if (atRest(character)) {
+    character->setAcceleration(10, UP);
   }
 }
 
-void downkey() {
+void downkey(Character* character) {
   // Nothing yet
 }
 
-void rightkey() {
+void rightkey(Character* character) {
   if (_heavyAttackTimer == 0) {
     _directionKeyTimer = 5;
-    _xVelocity = 100;
-    _facing = RIGHT;
+    character->setAcceleration(10, RIGHT);
+    character->setFacing(RIGHT);
   }
 }
 
-void leftkey() {
+void leftkey(Character* character) {
   if (_heavyAttackTimer == 0) {
     _directionKeyTimer = 5;
-    _xVelocity = -100;
-    _facing = LEFT;
+    character->setAcceleration(10, LEFT);
+    character->setFacing(LEFT);
   }
 }
 
-void nokey() {
+// TODO Requires some tuning to get desired behaviour. Idea is to accelerate opposite of movement direction until velocity reaches 0
+void nokey(Character* character) {
   if (_directionKeyTimer <= 0) {
     _directionKeyTimer = 0;
-    _xVelocity = 0;
+    character->setAcceleration(character->getXAcceleration(), character->getFacing());
   } else _directionKeyTimer--;
 }
 
-void akey() {
-  _yVelocity = -10;
+void akey(Character* character) {
+  character->setAcceleration(10, UP);
 }
 
-void skey() { // Heavy attack
+void skey(Character* character) { // Heavy attack
   if (_heavyAttackTimer == 0 && _lightAttackTimer == 0) _heavyAttackTimer = 70;
 }
 
-void dkey() { // Light attack
+void dkey(Character* character) { // Light attack
   if(_heavyAttackTimer == 0 && _lightAttackTimer == 0) _lightAttackTimer = 20;
 }
 
 void keyboardControl(int inputKey) {
   switch (inputKey) {
     case 65: // Up
-      upkey();
+      upkey(_player);
       break;
     case 66: // Down
-      downkey();
+      downkey(_player);
       break;
     case 67: // Right
-      rightkey();
+      rightkey(_player);
       break;
     case 68: // Left
-      leftkey();
+      leftkey(_player);
       break;
     case 97: // a
-      akey();
+      akey(_player);
       break;
     case 115: // s
-      skey();
+      skey(_player);
       break;
     case 100: // d
-      dkey();
+      dkey(_player);
       break;
     default:
-      nokey();
+      nokey(_player);
   }
 }
 
 void drawPlayer() {
-  if (_facing == RIGHT) mvprintw(_yPosition, _xPosition, ">");
-  else mvprintw(_yPosition, _xPosition, "<");
+  mvprintw(_player->getYPosition(), _player->getXPosition(), _player->getDrawChar().c_str());
 }
 
 void drawLevel() {
@@ -124,20 +120,20 @@ void drawLevel() {
 
 void draw() {
   drawPlayer();
-  mvprintw(1, 10, boost::lexical_cast<std::string>(_xVelocity).c_str());
-  mvprintw(2, 10, boost::lexical_cast<std::string>(_xAcceleration).c_str());
-  mvprintw(1, 30, boost::lexical_cast<std::string>(_yVelocity).c_str());
-  mvprintw(2, 30, boost::lexical_cast<std::string>(_yAcceleration).c_str());
-  mvprintw(3, 10, boost::lexical_cast<std::string>(_xPosition).c_str());
-  mvprintw(3, 30, boost::lexical_cast<std::string>(_yPosition).c_str());
+  mvprintw(1, 10, boost::lexical_cast<std::string>(_player->getXVelocity()).c_str());
+  mvprintw(2, 10, boost::lexical_cast<std::string>(_player->getXAcceleration()).c_str());
+  mvprintw(1, 30, boost::lexical_cast<std::string>(_player->getYVelocity()).c_str());
+  mvprintw(2, 30, boost::lexical_cast<std::string>(_player->getYAcceleration()).c_str());
+  mvprintw(3, 10, boost::lexical_cast<std::string>(_player->getXPosition()).c_str());
+  mvprintw(3, 30, boost::lexical_cast<std::string>(_player->getYPosition()).c_str());
   drawLevel();
 }
 
-void attack() {
+void attack(Character* character) {
   if (_heavyAttackTimer > 0)
-    _heavyAttackTimer = performHeavyAttack(_heavyAttackTimer, _facing, _yPosition, _xPosition);
+    _heavyAttackTimer = performHeavyAttack(_heavyAttackTimer, character->getFacing(), character->getYPosition(), character->getXPosition());
   if (_lightAttackTimer > 0)
-    _lightAttackTimer = performLightAttack(_lightAttackTimer, _facing, _yPosition, _xPosition);
+    _lightAttackTimer = performLightAttack(_lightAttackTimer, character->getFacing(), character->getYPosition(), character->getXPosition());
 }
 
 void loadLevel() {
@@ -157,23 +153,18 @@ void loadLevel() {
   infile.close();
 }
 
-float bounce(float velocity) {
-  float elasticity = 0.0;
-  return -elasticity*velocity;
-}
-
-void collisionDetect() {
-  int y = int(_yPosition);
-  int x = int(_xPosition);
-  if (y == HEIGHT) _yPosition = HEIGHT;
-  if (x == WIDTH) _xPosition = WIDTH;
+void collisionDetect(Character* character) {
+  int y = int(character->getYPosition());
+  int x = int(character->getXPosition());
+  if (y == HEIGHT) character->setYPosition(HEIGHT);
+  if (x == WIDTH) character->setXPosition(WIDTH);
   Block* block = _level[y+1][x];
-  if (block != 0 && !block->isPassableFromAbove() && _yVelocity > 0) {
-    _yPosition = y + 0.1;
-    _yVelocity = bounce(_yVelocity);
-  }
+  if (block != 0 && !block->isPassableFromAbove() && character->getYVelocity() > 0) {
+    character->setYPosition(y + 0.1);
+    character->setYVelocity(bounce(character->getYVelocity(), character->getElasticity()));
+  } // TODO: Store x and y velocity in local variables to avoid accessing over and over
   block = _level[y-1][x];
-  if (block != 0 && !block->isPassableFromBelow() && _yVelocity < 0) {
+  if (block != 0 && !block->isPassableFromBelow() && character->getYVelocity() < 0) {
     _yVelocity = bounce(_yVelocity);
   }
   block = _level[y][x+1];
@@ -204,6 +195,8 @@ int main(int argc, char *argv[]) {
   int inputKey = -1;
 
   _terminalVelocity = terminalVelocity();
+  
+  _player = new Character(10, 10, "<", ">", RIGHT);
 
   while(run) {
     clear();
