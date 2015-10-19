d70be6fd7ae86c3461e8b64936d6a8398abc0049
petterdj <petterdjupfeldt@gmail.com>
2015-10-19 21:35:15 +0200

    Created path class. The idea is that aiCharacters will have a path object they check to determine their next move. Up in the air if this will be kept or not.

---
+++ path.hpp
@@ -0,0 +1,20 @@
+#ifndef MYPATH
+#define MYPATH
+
+namespace terminalGame {
+class Path {
+protected:
+
+public:
+ // CONSTRUCTORS //
+
+ // OPERATORS //
+
+ // DESTRUCTOR //
+
+ // FUNCTIONS // 
+
+};
+}
+
+#endif
