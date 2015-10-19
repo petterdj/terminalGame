c49e3f2cd7f6f05f1e64ab090ab783e78049df72
petterdj <petterdjupfeldt@gmail.com>
2015-10-19 21:33:12 +0200

    Readded aiCharacter class. It is basically a normal character, except with NPC specific values, such as aggro range.

---
+++ aiCharacter.cpp
@@ -0,0 +1,58 @@
+#include "aiCharacter.hpp"
+
+using namespace terminalGame;
+
+// CONSTRUCTORS //
+AICharacter::AICharacter() :
+  Character() {
+    _aggroThreshold = 0;
+    //_path; // TODO Object or vector? It is basically an ordered set of instructions for reaching a specified point
+}
+
+
+AICharacter::AICharacter(const float y, const float x, const std::string leftDrawChar, 
+                         std::string rightDrawChar, const int facing, const float aggroThreshold,
+                         const float elasticity, const int shape) :
+  Character(y, x, leftDrawChar, rightDrawChar, facing, elasticity, shape) {
+  _aggroThreshold = aggroThreshold;
+}
+
+AICharacter::AICharacter(const AICharacter& o) :
+  Character(o) {
+  if (this == &o) return;
+  _aggroThreshold = o._aggroThreshold;
+}
+
+AICharacter::AICharacter(AICharacter&& o) :
+  Character(o) {
+  if (this == &o) return;
+  _aggroThreshold = o._aggroThreshold;
+  o._aggroThreshold = 0;
+}
+
+// OPERATORS //
+AICharacter& AICharacter::operator=(const AICharacter& o) {
+  if (this == &o) return *this;
+  Character::operator=(o);
+  _aggroThreshold = o._aggroThreshold;
+  return *this;
+}
+
+AICharacter& AICharacter::operator=(AICharacter&& o) {
+  if (this == &o) return *this;
+  Character::operator=(o);
+  _aggroThreshold = o._aggroThreshold;
+  o._aggroThreshold = 0;
+  return *this;
+}
+
+// DESTRUCTOR //
+AICharacter::~AICharacter() {
+  _aggroThreshold = 0;
+}
+
+// FUNCTIONS //
+float AICharacter::getAggroThreshold() const { return _aggroThreshold; }
+
+void AICharacter::setPath(Path p) { _path = p; }
+
---
+++ aiCharacter.hpp
@@ -0,0 +1,37 @@
+#ifndef MYAICHARACTER
+#define MYAICHARACTER
+#include "character.hpp"
+#include "path.hpp"
+#include <iostream>
+
+namespace terminalGame {
+
+  class AICharacter : public Character {
+    protected:
+      float _aggroThreshold;
+      Path _path; // TODO Object or vector? It is basically an ordered set of instructions for reaching a specified point
+
+    public:
+      // CONSTRUCTORS //
+      AICharacter();
+      AICharacter(const float y, const float x, const std::string leftDrawChar, 
+        const std::string rightDrawChar, const int facing, 
+        const float aggroThreshold = 10.0, const float elasticity=0.0, 
+        const int shape=SPHERE);
+      AICharacter(const AICharacter&);
+      AICharacter(AICharacter&&);
+
+      // OPERATORS //
+      AICharacter& operator=(const AICharacter&);
+      AICharacter& operator=(AICharacter&&);
+
+      // DESTRUCTOR //
+      ~AICharacter();
+
+      // FUNCTIONS //
+      float getAggroThreshold() const;
+      
+      void setPath(const Path);
+  };
+}
+#endif
